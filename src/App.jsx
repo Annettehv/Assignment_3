9aff0fda9a618e7364514deea3e82e95732ad621
Annettehv <anha040@student.kristiania.no>
2024-05-01 23:23:38 +0200

    clean up files

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 import ExpenseTracker from './assets/ExpenseTracker'
-import ExpenseForm from './assets/ExpenseForm'
+
 
 function App() {
   const [count, setCount] = useState(0)
